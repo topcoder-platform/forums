c524dc4c3fd558e4c1e61d4e4ebc3d516c3fa969
Bogdanova Olga <o_bogdanova@inbox.ru>
2020-09-28 22:18:10 +0300

    Enabled Filestack

--- config/vanilla/config.php
+++ config/vanilla/config.php
@@ -31,7 +31,7 @@ $Configuration['EnabledPlugins']['Quotes'] = true;
 $Configuration['EnabledPlugins']['swagger-ui'] = true;
 $Configuration['EnabledPlugins']['oauth2'] = false;
 $Configuration['EnabledPlugins']['Groups'] = true;
-$Configuration['EnabledPlugins']['Filestack'] = false;
+$Configuration['EnabledPlugins']['Filestack'] = true;
 
 // Debug
 $Configuration['Debug'] = FALSE;
