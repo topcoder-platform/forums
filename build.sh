628f509d42bf88f2be980456d7de49eb427944c5
Bogdanova Olga <o_bogdanova@inbox.ru>
2020-12-06 22:21:43 +0300

    Issues-265

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -eo pipefail
 APP_NAME=$1
 CI_DEPLOY_TOKEN=$2
+ENV=$3
 UPDATE_CACHE=""
 echo "" > vanilla.env
-CI_DEPLOY_TOKEN=$CI_DEPLOY_TOKEN docker-compose -f docker-compose.yml build $APP_NAME
+ENV=$ENV CI_DEPLOY_TOKEN=$CI_DEPLOY_TOKEN docker-compose -f docker-compose.yml build $APP_NAME
 #docker create --name app $APP_NAME:latest
